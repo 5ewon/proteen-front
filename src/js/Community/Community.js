1dd2ea9af55a1294e00d31eefd7dd38e7cdde29f
Gvvon5ewon <gwonsewon03@gmail.com>
2022-09-22 20:36:45 +0900

    중간저장

--- src/js/Community/Community.js
+++ src/js/Community/Community.js
@@ -1,7 +1,23 @@
-function Community(){
-    return(
-        <div>community</div>
+import './Community.scss';
+
+const Community = () => {
+  return (
+    <div id="community-container">
+        <div id="community">
+            <div id="float-post">
+                <div>답변을 기다리고 있어요</div>
+                <div>
+                    <ul className="postList">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
     );
-}
+};
 
 export default Community;
